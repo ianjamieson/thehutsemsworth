e7c7153b8e0b1dc70c5d9ded24a62f15a59ec324
Ian Jamieson <ian@tixel.com>
2022-10-02 00:05:46 +0100

    Initial website design

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./docs/**/*.{html,js}'],
+    theme: {
+      colors: {
+        black: 'black',
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'blue': {
+          100: '#C2DFE9',
+          200: '#9FBBC5',
+          300: '#7D98A2',
+          400: '#5C7780',
+          500: '#3d5760',
+        },
+      },
+      extend: {
+        maxWidth: {
+          container: '1024px'
+        }
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ]
+  }
